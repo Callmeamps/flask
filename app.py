f021e8c22b0970713c8eddaffa3189fee5c9007b
Callmeamps <95033065+Callmeamps@users.noreply.github.com>
2023-05-04 13:01:52 +0000

    init

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template, request
+from flask import request
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/welcome", methods=["POST"])
+def welcome():
+    name = request.args.get("username", "World")
+    return render_template("welcome.html", name=name)
